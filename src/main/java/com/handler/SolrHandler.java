0cf84f94e2cc531df047157c55d96a37ae4bf33e
lewisli <lewisli.acer@gmail.com>
2015-05-08 18:31:47 +0800

    test code

---
+++ src/main/java/com/handler/SolrHandler.java
@@ -0,0 +1,14 @@
+package com.handler;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+ 
+public class SolrHandler {
+ 
+    static Log logger = LogFactory.getLog(SolrHandler.class);
+ 
+    public static void main(String[] args) {
+        logger.info("Hello World");
+    }
+ 
+}
